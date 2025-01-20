40bf12fee45b257e6e1f6cee2d15db0f91d014b5
damonkost <damonkost@gmail.com>
2025-01-20 11:34:22 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import App from "./App";
-import Button from "./Button";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+// Render application
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// Render the Button component in the react-button div
-const buttonRoot = ReactDOM.createRoot(document.getElementById("react-button"));
-buttonRoot.render(
-  <Button label="Get Started" onClick={() => alert("Button clicked!")} isLoading={false} disabled={false} />
 );

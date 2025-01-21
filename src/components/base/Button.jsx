949d914b88132c8503a94dd5f5769acb01d58911
damonkost <damonkost@gmail.com>
2025-01-20 20:57:34 -0500

    Update Button.jsx

--- src/components/base/Button.jsx
+++ src/components/base/Button.jsx
@@ -3,7 +3,10 @@ import ScaleLoader from "react-spinners/ScaleLoader";
 
 const Button = ({ label, onClick, isLoading, disabled = false }) => {
   const opacity = disabled ? 0.75 : 1;
+  const cursor = disabled ? : "pointer";
 
+  const handleCheckboxChange = () => {
+    setAgreedToTerms(!agreedToTerms);
   const Contents = isLoading ? (
     <ScaleLoader
       color="#000"

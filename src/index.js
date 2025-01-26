2a39082843716ed255a2e96b86e4d9b51610625e
damonkost <damonkost@gmail.com>
2025-01-26 11:26:33 -0500

    Update rendering method to use React 18's createRoot for improved performance

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import "./index.css";
 
 import App from "./App";
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 // Render application
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );

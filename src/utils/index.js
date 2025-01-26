8178abb11c9fb8aee741a420ac11b7120cf7b95c
damonkost <damonkost@gmail.com>
2025-01-26 13:39:57 -0500

    fix: standardize whitespace in CSS and add console log in isPublicKeyMissingError function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export const isPublicKeyMissingError = ({ vapiError }) => {
+  console.log("index.js is loaded");
   return !!vapiError && vapiError.error.statusCode === 403 && vapiError.error.error === "Forbidden";
 };

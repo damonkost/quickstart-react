5a7d10edf7c874209ee280b8d0185feb8b3a8c3e
damonkost <damonkost@gmail.com>
2025-01-27 18:08:32 -0500

    Remove ErrorBoundary component from App component

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import '../src/styles/index.css'; // Import global CSS
-import ErrorBoundary from '../src/components/ErrorBoundary';
+// import ErrorBoundary from '../src/components/ErrorBoundary';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ErrorBoundary>
+    // <ErrorBoundary>
       <Component {...pageProps} />
-    </ErrorBoundary>
+    // </ErrorBoundary>
   );
 }
 

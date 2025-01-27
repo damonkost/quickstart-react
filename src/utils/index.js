f027c01632251a33c3e19308416c8aef76693af5
damonkost <damonkost@gmail.com>
2025-01-27 08:31:55 -0500

    feat: integrate ReactDOM rendering logic into utils for application entry point

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,16 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
 export const isPublicKeyMissingError = ({ vapiError }) => {
     return !!vapiError && vapiError.error.statusCode === 403 && vapiError.error.error === "Forbidden";
 };
-```
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+  

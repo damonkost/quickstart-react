b76fe62f9f755a6374f5a4780ba63c645ac6fffd
damonkost <damonkost@gmail.com>
2025-01-27 15:49:41 -0500

    refactor(package): migrate from Create React App to Next.js project structure

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-import App from '../src/App';
+import dynamic from 'next/dynamic';
+
+// Dynamically import App with no SSR since it uses window
+const App = dynamic(() => import('../src/App'), { ssr: false });
 
 export default function Home({ attorneyProfile }) {
   return <App initialProfile={attorneyProfile} />;
-} 
+}
+
+// This ensures the page is rendered on the client side
+export const getServerSideProps = async () => {
+  return {
+    props: {}
+  };
+}; 

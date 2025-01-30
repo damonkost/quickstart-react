1c0d42aa219d21ab1d1761188ceaf74311e3c242
damonkost <damonkost@gmail.com>
2025-01-30 11:46:18 -0500

    Update viewport meta tag and favicon link; refactor index.js import order

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import "./index.css";
-import App from "./app";
-import React from "react";
-import ReactDOM from "react-dom/client";
+// filepath: src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-// Render application
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>

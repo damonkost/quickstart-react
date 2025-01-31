042cb9d5c23bcb7f6f39d4bcc44f826a09b7d551
damonkost <damonkost@gmail.com>
2025-01-30 21:59:41 -0500

    update depend

--- next.config.js
+++ next.config.js
@@ -2,24 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Client-side only
-      config.module.rules.push({
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      });
-    }
+  // Handle CSS imports
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    });
     return config;
-  },
-  // Ensure pages are properly exported
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  // Add trailing slash to URLs
-  trailingSlash: true,
-  // Enable source maps in development
-  productionBrowserSourceMaps: true,
+  }
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
 import '../src/styles/index.css';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>LegalScout</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />
 }
 
 export default MyApp;

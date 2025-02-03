1f668205ec8bf8ae0a149ebcec50342d7a92e7c2
damonkost <damonkost@gmail.com>
2025-02-03 06:44:19 -0500

    Update Vercel project configuration and yarn.lock dependencies
    - Expand Vercel project.json with additional settings
    - Update yarn.lock with latest Next.js and React-related dependencies
    - Remove redundant version constraints for various packages

---
+++ .vercel/output/static/_next/static/Y67RoCT3tg4Q4uA-GxALX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/4ec946c7-2a5f360213da3f17.js","static/chunks/pages/index-109abf1f8c2ceaa2.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
